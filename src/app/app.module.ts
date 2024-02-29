fa2b898c523fd1a48302f5e3b2bc8a74695eb1e5
Md. Farid <md.farid@Mds-MacBook-Pro.local>
2024-02-29 20:00:40 +0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularSplitModule } from 'angular-split';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { QuestionComponent } from './views/question/question.component';
+import { QthumbnailComponent } from './views/qthumbnail/qthumbnail.component';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    QuestionComponent,
+    QthumbnailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularSplitModule,
+    FormsModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
