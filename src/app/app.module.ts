c1bad50f54712c1c7d75a858f1cb1d85185100b8
BodrulAmin <bodrulaminiu@gmail.com>
2024-03-02 00:32:55 +0600

    added prime ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import {BrowserModule} from "@angular/platform-browser";
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [
   ],
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,13 +1,17 @@
 import {NgModule} from '@angular/core';
 import {AdminLoginComponent} from './components/admin-login/admin-login.component';
 import {RouterModule} from "@angular/router";
-
+import {adminRoutes} from "./admin.route";
+import {InputTextModule} from "primeng/inputtext";
+import { CheckboxModule } from 'primeng/checkbox';
+import { RadioButtonModule } from 'primeng/radiobutton';
 
 @NgModule({
   imports: [
-    RouterModule.forChild([
-      {path: '', component: AdminLoginComponent}
-    ])
+    RouterModule.forChild(adminRoutes),
+    InputTextModule,
+    CheckboxModule,
+    RadioButtonModule,
   ],
   declarations: [AdminLoginComponent]
 })
